a216c23de6054f631bee71918f01ec15dbc0b5b1
lukasmas <mastalerzl12@gmail.com>
2021-01-06 16:44:12 +0100

    added support for DCT method 50%

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import cv2
 from PySide2 import QtWidgets
 from PySide2.QtCore import QThreadPool, QCoreApplication, QRect, QMetaObject
 
+from WatermarkDCT import WatermarkDCT
 from gui_fns import *
 from watermarker import LSBWatermarker
 from worker import Worker
@@ -15,13 +16,19 @@ from PySide2.QtWidgets import (QLineEdit, QPushButton, QApplication,
 
 import sys
 
+
 class Ui_MainWindow(object):
+
+    def __init__(self):
+        self.file_name = ""
+        self.threadpool = QThreadPool()
+        self.num_of_co = 1000
+        self.alpha = 0.1
+
     def setupUi(self, MainWindow):
         if not MainWindow.objectName():
             MainWindow.setObjectName(u"MainWindow")
         MainWindow.resize(1024, 768)
-        self.file_name = ""
-        self.threadpool = QThreadPool()
         self.centralwidget = QWidget(MainWindow)
         self.centralwidget.setObjectName(u"centralwidget")
         self.verticalLayoutWidget_2 = QWidget(self.centralwidget)
@@ -73,7 +80,6 @@ class Ui_MainWindow(object):
 
         self.verticalLayout_3.addWidget(self.DCT_masg)
 
-
         self.horizontalLayout_2.addLayout(self.verticalLayout_3)
 
         self.verticalLayout = QVBoxLayout()
@@ -97,7 +103,6 @@ class Ui_MainWindow(object):
         self.lsb_encode_msg.setObjectName(u"lsb_encode_msg")
 
         self.verticalLayout.addWidget(self.lsb_encode_msg)
-
 
         self.LSB_msg = QLineEdit(self.horizontalLayoutWidget)
         self.LSB_msg.setObjectName(u"LSB_msg")
@@ -137,11 +142,14 @@ class Ui_MainWindow(object):
         self.picture_button.clicked.connect(self.open_file)
         self.LSB_encode.clicked.connect(self.call_lsb_encode)
         self.LSB_decode.clicked.connect(self.call_lsb_decode)
-        # self.bindFunctions()
+        self.DCT_encode.clicked.connect(self.call_dct_encode)
+        self.DCT_decode.clicked.connect(self.call_dct_decode)
+        # end bind functions()
         self.hide_progress_bar()
         self.disable_Methods(True)
 
         QMetaObject.connectSlotsByName(MainWindow)
+
     # setupUi
 
     def retranslateUi(self, MainWindow):
@@ -152,13 +160,14 @@ class Ui_MainWindow(object):
         self.DCT_encode.setText(QCoreApplication.translate("MainWindow", u"encode", None))
         self.DCT_decode.setText(QCoreApplication.translate("MainWindow", u"decode", None))
         self.label_4.setText(QCoreApplication.translate("MainWindow", u"coeficient:", None))
-        self.DCT_masg.setText(QCoreApplication.translate("MainWindow", u"num_of_co=1000, alpha=0.1", None))
+        self.DCT_masg.setText(QCoreApplication.translate("MainWindow", u"1000, 0.1", None))
         self.label_3.setText(QCoreApplication.translate("MainWindow", u"LSB", None))
         self.LSB_encode.setText(QCoreApplication.translate("MainWindow", u"encode", None))
         self.LSB_decode.setText(QCoreApplication.translate("MainWindow", u"decode", None))
         self.lsb_encode_msg.setText(QCoreApplication.translate("MainWindow", u"LSB msg:", None))
         self.LSB_msg.setText(QCoreApplication.translate("MainWindow", u"secret", None))
         self.label_6.setText(QCoreApplication.translate("MainWindow", u"Work in progress:", None))
+
     # retranslateUi
 
     # def bindFunctions(self):
@@ -194,7 +203,6 @@ class Ui_MainWindow(object):
 
     def method_LSB_decode(self, progress_callback):
         self.disable_Methods(True)
-        self.lsb_encode_msg.setText("Decoded msg:")
         self.LSB_msg.setText("")
         progress_callback.emit(10)
         image = cv2.imread(self.file_name[0])
@@ -204,9 +212,45 @@ class Ui_MainWindow(object):
         self.disable_Methods(False)
         progress_callback.emit(90)
         # self.show_decoded_msg(watermarker.decoded_msg)
-        self.LSB_msg.setText("Decoded: " +watermarker.decoded_msg)
+        self.LSB_msg.setText("Decoded: " + watermarker.decoded_msg)
         progress_callback.emit(100)
 
+    def method_DCT_encode(self, progress_callback):
+        self.disable_Methods(True)
+        progress_callback.emit(10)
+        msg = self.DCT_masg.text().split(",")
+        try:
+            self.num_of_co = int(msg[0])
+            self.alpha = float(msg[1])
+            watermarker = WatermarkDCT(self.file_name[0], num_of_co=self.num_of_co, alpha=self.alpha)
+            progress_callback.emit(50)
+            watermarker.encode_watermark()
+            self.DCT_masg.setText("Picture is watermarked?:" + str(watermarker.detect_watermark()))
+        except Exception as e:
+            print(e)
+            self.DCT_masg.setText("Error: wrong parameters: example 1000, 0.1")
+        finally:
+            progress_callback.emit(90)
+            self.load_picture()
+            self.disable_Methods(False)
+            progress_callback.emit(100)
+
+    def method_DCT_decode(self, progress_callback):
+        self.disable_Methods(True)
+        progress_callback.emit(10)
+        try:
+            watermarker = WatermarkDCT(self.file_name[0], num_of_co=self.num_of_co, alpha=self.alpha)
+            progress_callback.emit(50)
+            self.DCT_masg.setText("Picture is watermarked?:" + str(watermarker.detect_watermark(self.file_name[0])))
+        except Exception as e:
+            print(e)
+            self.DCT_masg.setText("Error: Unable to check the picture.")
+        finally:
+            progress_callback.emit(90)
+
+            self.load_picture()
+            self.disable_Methods(False)
+            progress_callback.emit(100)
 
     def show_decoded_msg(self, msg):
         msgBox = QMessageBox()
@@ -225,19 +269,22 @@ class Ui_MainWindow(object):
 
     def call_lsb_encode(self):
         self.show_progress_bar()
-        # Pass the function to execute
-        worker = Worker(self.method_LSB_encode)  # Any other args, kwargs are passed to the run function
-        worker.signals.result.connect(self.print_output)
-        worker.signals.finished.connect(self.thread_complete)
-        worker.signals.progress.connect(self.progress_fn)
-
-        # Execute
-        self.threadpool.start(worker)
+        self.call_function(self.method_LSB_encode)
 
     def call_lsb_decode(self):
         self.show_progress_bar()
-        # Pass the function to execute
-        worker = Worker(self.method_LSB_decode)  # Any other args, kwargs are passed to the run function
+        self.call_function(self.method_LSB_decode)
+
+    def call_dct_encode(self):
+        self.show_progress_bar()
+        self.call_function(self.method_DCT_encode)
+
+    def call_dct_decode(self):
+        self.show_progress_bar()
+        self.call_function(self.method_DCT_decode)
+
+    def call_function(self, fn):
+        worker = Worker(fn)  # Any other args, kwargs are passed to the run function
         worker.signals.result.connect(self.print_output)
         worker.signals.finished.connect(self.thread_complete)
         worker.signals.progress.connect(self.progress_fn)
